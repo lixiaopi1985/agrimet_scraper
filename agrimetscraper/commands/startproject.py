40198c26ca2dfc4a35081d3439ac851449efe673
lixiaopi1985 <lixiaopi@oregonstate.edu>
2019-02-20 18:53:46 -0800

    change agrimet_scraper to agrimetscraper

---
+++ agrimetscraper/commands/startproject.py
@@ -0,0 +1,6 @@
+"""
+start scraper project
+"""
+
+import os
+import sys
---
+++ agrimetscraper/utils/loader.py
@@ -0,0 +1,11 @@
+from agrimetscraper import configuration
+from configparser import RawConfigParser
+
+
+def loadconfig():
+
+	print(base_settings)
+
+
+if __name__ == '__main__':
+	print(loadconfig())
